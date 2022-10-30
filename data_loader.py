3df0b13585f1b582ee6c8476c30444f4a3b56357
Kfried <phil.bridge@ixapps.co.uk>
2022-10-30 22:24:37 +0000

    Refactoring, implement parameter validation all channels

--- data_loader.py
+++ data_loader.py
@@ -8,13 +8,31 @@ def load_data(type, params):
 
 
 def load_from_csv(params):
-    pass
+    if verify_params(params, ['database','collection','filename'], {'passed':'load from csv params good','failed':'load from csv parameter error, expect database, collection, filename'}):
+        #run load from file logic
+        return True
+    else:
+        return False
 
 def load_from_api(params):
-    pass
+    if verify_params(params, ['source'],{'passed':'load from api params good','failed':'load from api parameter error, expect source | optional dates'}):
+        #run load from api logic
+        return True
+    else:
+        return False
 
 def load_from_mongo(params):
-    pass
+    if verify_params(params, ['collection'],{'passed':'load from mongo params good','failed':'load from mongo parameter error, expect collection | optional dates'}):
+        #run load from mongo logic
+        return True
+    else:
+        return False
+
 
 def verify_params(params, validation, messages):
-    pass
+    if all (item in validation for item in params):
+        print(messages['passed'])
+        return True
+    else:
+        print(messages['failed'])
+        return False
