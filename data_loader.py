810bdc603a0f6a176a8a692d97fc7822c8ae4966
Kfried <phil.bridge@ixapps.co.uk>
2022-10-30 21:20:06 +0000

    refactoring skeleton load module added

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,8 @@ def load_data(type, params):
         return load_from_csv(params)
     elif type == 'api':
         return load_from_api(params)
+    elif type == 'mongo':
+        return load_from_mongo(params)
 
 
 def load_from_csv(params):
@@ -12,4 +14,7 @@ def load_from_api(params):
     pass
 
 def load_from_mongo(params):
+    pass
+
+def verify_params(params, validation, messages):
     pass
